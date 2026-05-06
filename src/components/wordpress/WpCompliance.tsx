f89a1c8a72376aef17e5718cab5d09818c1f4e48
C9 Mayur <mayur@c9communications.com.au>
2026-05-06 13:28:35 +0530

    perf: convert static components to Server Components to reduce JS bundle size

--- src/components/wordpress/WpCompliance.tsx
+++ src/components/wordpress/WpCompliance.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { ShieldCheck, Database, Lock, Headset } from 'lucide-react';
 
--- src/components/wordpress/WpSupport.tsx
+++ src/components/wordpress/WpSupport.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import React from 'react';
 import { CheckCircle } from 'lucide-react';
 import { Button } from '@/components/ui/button';
